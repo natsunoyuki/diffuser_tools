2aad38a698f2b40e527b0d05bfa88034f2434eaf
Ненавин <33706936+natsunoyuki@users.noreply.github.com>
2025-06-25 12:30:06 +0800

    Inserted GPL3.0 license information in source code files.

--- src/diffuser_tools/prompt_utilities.py
+++ src/diffuser_tools/prompt_utilities.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Natsunoyuki AI Laboratory
+# Copyright 2025 Natsunoyuki.
 
 # diffuser_tools is free software: you can redistribute it and/or modify it 
 # under the terms of the GNU General Public License as published by the Free 
