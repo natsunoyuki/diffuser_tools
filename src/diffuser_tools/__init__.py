fb7d41d55d5be7138d43a5e7d4c2ef3d8fa11e9a
Ненавин <33706936+natsunoyuki@users.noreply.github.com>
2025-06-25 12:28:41 +0800

    Inserted GPL3.0 license information in source code files.

--- src/diffuser_tools/__init__.py
+++ src/diffuser_tools/__init__.py
@@ -1,5 +1,17 @@
-# Package: diffuser_tools
-# Author: Y. Natsume
+# Copyright 2025 Natsunoyuki AI Laboratory
+
+# diffuser_tools is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the Free 
+# Software Foundation, either version 3 of the License, or (at your option) any 
+# later version.
+
+# diffuser_tools is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+# details.
+
+# You should have received a copy of the GNU General Public License along with 
+# diffuser_tools. If not, see <https://www.gnu.org/licenses/>.
 
 from diffuser_tools import (
     img2img_utilities, 
